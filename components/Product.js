fe68efb712f3c6c938b1e387ef66fb4bcd89ebb4
Ayesha Khalid <ayeshakhalid289@gmail.com>
2024-03-28 14:14:27 +0500

    #70 Make Action

--- components/Product.js
+++ components/Product.js
@@ -1,14 +1,21 @@
 import React from "react";
 import {View,Text, Image, Button, StyleSheet} from "react-native"
+import { addToCart } from "./redux/action";
+import { UseDispatch, useDispatch } from "react-redux";
 
 const Product =(props)=>{
     const item=props.product
+    const dispatch=useDispatch();
+    const handleAddToCart=()=>{
+
+    }
+
     return(
         <View style={styles.main}>
             <Text style={{fontSize:20,fontWeight:"bold"}}>{item.name}</Text>
             <Text style={{fontSize:15}}>{item.detail}</Text>
             <Image style={{width:150, height:150,marginBottom:20}} source={{uri:item.image}}/>
-            <Button title="add to cart"></Button>
+            <Button title="add to cart" onPress={()=>dispatch(addToCart(item))}></Button>
         </View>
     );
 }
