ba2d80569ba983d2bc6035696e3e0508fe1d9553
Ayesha Khalid <ayeshakhalid289@gmail.com>
2024-04-04 00:38:20 +0500

    Movie Screen poster added

--- components/MovieApp/screens/MovieScreen.js
+++ components/MovieApp/screens/MovieScreen.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, ScrollView, Touchable, Image, TouchableOpacity, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, StatusBar, Image, TouchableOpacity, Dimensions, Platform } from 'react-native';
 import { useRoute } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { primary, theme } from "../theme";
 import { ChevronLeftIcon } from "react-native-heroicons/outline";
 import { HeartIcon } from "react-native-heroicons/solid"
 import { useNavigation } from "@react-navigation/native";
-import { LinearGradient } from "react-native-svg";
+import LinearGradient from "react-native-linear-gradient";
 
 const MovieScreen = () => {
     // const {params}=props.route.params //not corret
@@ -14,12 +14,15 @@ const MovieScreen = () => {
     const { params: item } = useRoute()
     const [isFavourite, toggleFavourite] = useState(false)
     const navigation = useNavigation()
+    const ios =Platform.OS=='ios';
+    const moviename="Marvel Avengers 3"
     return (
 
-        <ScrollView style={styles.container}>
+        <ScrollView contentContainerStyle={{paddingBottom:20}} style={styles.container}>
             {/* back button and movie poster */}
-            <View>
-                <SafeAreaView style={[styles.header, { padding: 10 }]}>
+            <View style={{width:"100%"}}>
+                <SafeAreaView style={[styles.header, { paddingHorizontal: 16,marginTop:ios?"":12 }]}>
+                <StatusBar barStyle={'light-content'} backgroundColor="transparent" translucent/>
                     <TouchableOpacity onPress={() =>navigation.goBack()} style={[primary.background, { padding: 5, borderRadius: 12 }]}>
                         <ChevronLeftIcon size={28} strokeWidth={2.5} color="white"></ChevronLeftIcon>
                     </TouchableOpacity>
@@ -29,7 +32,16 @@ const MovieScreen = () => {
                 </SafeAreaView>
                 <View>
                     <Image source={{uri:"https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"}} style={{width:width,height:height*0.55}}/>
+                <LinearGradient colors={['transparent','rgba(23,23,23,.9)','rgba(23,23,23,1)']}
+                style={{width:width,height:height*0.44,position:"absolute",bottom:0}}
+                start={{x:0.5,y:0}}
+                end={{x:0.5,y:1}}
+                />
                 </View>
+            </View>
+            {/* movie details */}
+            <View style={{marginTop:-(height*0.09)}}>
+                <Text style={{fontSize:28,fontWeight:"bold",color:"white",textAlign:"center"}}>{moviename}</Text>
             </View>
         </ScrollView>
 
@@ -38,7 +50,7 @@ const MovieScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "rgb(38 38 38)",
+        backgroundColor: "rgb(23 23 23)"
     },
     header: {
         width:'100%',
