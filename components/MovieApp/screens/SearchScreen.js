894e69d4f5964d35c10632670af9c34a2b74c022
Ayesha Khalid <ayeshakhalid289@gmail.com>
2024-04-04 23:25:10 +0500

    create search bar and show movie list

--- components/MovieApp/screens/SearchScreen.js
+++ components/MovieApp/screens/SearchScreen.js
@@ -1,24 +1,52 @@
-import { View, Text,StyleSheet, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Image,Text, StyleSheet, TextInput, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Dimensions } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { XMarkIcon } from 'react-native-heroicons/outline';
 import { useNavigation } from "@react-navigation/native";
 export default function SearchScreen() {
-    const navigation=useNavigation()
-  return (
-    <SafeAreaView style={styles.container}>
-        <View style={styles.mainS}>
-            <TextInput
-            placeholder='Search Movie'
-            placeholderTextColor={"lightgray"}
-            style={styles.input}
-            ></TextInput>
-            <TouchableOpacity onPress={()=>{navigation.navigate("Home")}}>
-                <XMarkIcon size={25} color="white"/>
-            </TouchableOpacity>
-        </View>
-    </SafeAreaView>
-  )
+    const navigation = useNavigation()
+    const [result, setResult] = useState([1, 2, 3, 4, 5]);
+    const { width, height } = Dimensions.get("window");
+    const moviename = "Harry Potter and the Philosopher's Stone";
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.mainS}>
+                <TextInput
+                    placeholder='Search Movie'
+                    placeholderTextColor={"lightgray"}
+                    style={styles.input}
+                ></TextInput>
+                <TouchableOpacity onPress={() => { navigation.navigate("Home") }}>
+                    <XMarkIcon size={25} color="white" />
+                </TouchableOpacity>
+            </View>
+            <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingHorizontal: 15 }}>
+                <Text style={styles.result}>Result ({result.length})</Text>
+                <View style={{ flexDirection: "row", justifyContent: "space-between", flexWrap: "wrap" }}>
+                   
+                    {
+                        result.map((item, index) => {
+                            return (
+                                <TouchableWithoutFeedback
+                                    key={index}
+                                    onPress={() => navigation.push("Movie", item)}
+                                >
+
+                                    <View>
+                                        <Image
+                                            style={{ width: width * 0.44, height: height * 0.3,borderRadius:20 }}
+                                            source={{ uri: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" }} />
+                                     <Text ellipsizeMode='tail' numberOfLines={1} style={styles.title}>{moviename}</Text>
+                                    </View>
+                                </TouchableWithoutFeedback>
+                            )
+                        })
+
+                    }
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -28,20 +56,28 @@ const styles = StyleSheet.create({
     },
     mainS: {
         // paddingVertical:0,
-        margin:10,
-        paddingHorizontal:15,
-        borderWidth:1,
-        borderRadius:30,
-        borderColor:"rgb(115 115 115)",
-        
+        margin: 10,
+        paddingHorizontal: 15,
+        borderWidth: 1,
+        borderRadius: 30,
+        borderColor: "rgb(115 115 115)",
+
         flexDirection: "row",
         justifyContent: "center",
         alignItems: "center"
     },
-    input:{
-        flex:1,
-        fontSize:16,
-        color:"white",
-       
-    }
+    input: {
+        flex: 1,
+        fontSize: 16,
+        color: "white",
+
+    },
+    result: {
+        color: "white",
+        marginBottom: 10,
+        fontSize: 16
+    },
+    title: {
+        width:150,marginBottom: 16,marginLeft:10, marginTop:8, fontSize: 15, color: "rgb(163 163 163)", fontWeight: "400"
+    },
 });
