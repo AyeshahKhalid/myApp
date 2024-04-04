2b5e1ca170743145f14814b92bb3da354127e2bd
Ayesha Khalid <ayeshakhalid289@gmail.com>
2024-04-05 00:26:51 +0500

    creates zero state in search screen

--- components/MovieApp/screens/SearchScreen.js
+++ components/MovieApp/screens/SearchScreen.js
@@ -1,11 +1,12 @@
-import { View, Image,Text, StyleSheet, TextInput, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Dimensions } from 'react-native'
+import { View, Image, Text, StyleSheet, TextInput, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Dimensions } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { XMarkIcon } from 'react-native-heroicons/outline';
 import { useNavigation } from "@react-navigation/native";
+
 export default function SearchScreen() {
     const navigation = useNavigation()
-    const [result, setResult] = useState([1, 2, 3, 4, 5]);
+    const [result, setResult] = useState([]);
     const { width, height } = Dimensions.get("window");
     const moviename = "Harry Potter and the Philosopher's Stone";
     return (
@@ -20,31 +21,45 @@ export default function SearchScreen() {
                     <XMarkIcon size={25} color="white" />
                 </TouchableOpacity>
             </View>
-            <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingHorizontal: 15 }}>
-                <Text style={styles.result}>Result ({result.length})</Text>
-                <View style={{ flexDirection: "row", justifyContent: "space-between", flexWrap: "wrap" }}>
-                   
-                    {
-                        result.map((item, index) => {
-                            return (
-                                <TouchableWithoutFeedback
-                                    key={index}
-                                    onPress={() => navigation.push("Movie", item)}
-                                >
+            {
+                result.length ?
+                    <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingHorizontal: 15 }}>
+                        <Text style={styles.result}>Result ({result.length})</Text>
+                        <View style={{ flexDirection: "row", justifyContent: "space-between", flexWrap: "wrap" }}>
 
-                                    <View>
-                                        <Image
-                                            style={{ width: width * 0.44, height: height * 0.3,borderRadius:20 }}
-                                            source={{ uri: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" }} />
-                                     <Text ellipsizeMode='tail' numberOfLines={1} style={styles.title}>{moviename}</Text>
-                                    </View>
-                                </TouchableWithoutFeedback>
-                            )
-                        })
+                            {
+                                result.map((item, index) => {
+                                    return (
+                                        <TouchableWithoutFeedback
+                                            key={index}
+                                            onPress={() => navigation.push("Movie", item)}
+                                        >
 
-                    }
-                </View>
-            </ScrollView>
+                                            <View>
+                                                <Image
+                                                    style={{ width: width * 0.44, height: height * 0.3, borderRadius: 20 }}
+                                                    source={{ uri: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" }} />
+                                                <Text ellipsizeMode='tail' numberOfLines={1} style={styles.title}>{moviename}</Text>
+                                            </View>
+                                        </TouchableWithoutFeedback>
+                                    )
+                                })
+
+                            }
+                        </View>
+                    </ScrollView>
+                    :
+                    <View style={{
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: "center",
+
+
+                    }}>
+                        <Image style={{ width: width * 0.7, height: height * 0.3 }} source={require("../../MovieApp/assets/images/undraw_raining.png")} />
+                    </View>
+            }
+
         </SafeAreaView>
     )
 }
@@ -78,6 +93,6 @@ const styles = StyleSheet.create({
         fontSize: 16
     },
     title: {
-        width:150,marginBottom: 16,marginLeft:10, marginTop:8, fontSize: 15, color: "rgb(163 163 163)", fontWeight: "400"
+        width: 150, marginBottom: 16, marginLeft: 10, marginTop: 8, fontSize: 15, color: "rgb(163 163 163)", fontWeight: "400"
     },
 });
